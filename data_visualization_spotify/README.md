280b012566cf7ccefa0d3c9a8ce75723e7f107dc
oliviajinwang <oliviajinwang@gmail.com>
2026-01-04 11:27:09 -0800

    First visualization of the project. This change includes a short description of the project, the source data file, a python program to render the top 10 popular Spotify tracks from 2009-2025, as well as the graph itself.

---
+++ data_visualization_spotify/README.md
@@ -0,0 +1,6 @@
+This project explores the visualization of popular tracks and artists on Spotify between 2009-2025.
+
+Our data source is from Kaggle.
+https://www.kaggle.com/datasets/wardabilal/spotify-global-music-dataset-20092025
+
+We used Python libraries, including pandas and matplotlib, to visualize trends in the data.
---
+++ data_visualization_spotify/spotify_top_tracks.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load data
+datafile = pd.read_csv("data_visualization_spotify\spotify_data_clean.csv")
+
+sort_column = 'track_popularity'
+ascending_order = False
+datafile.sort_values(
+    by=sort_column,
+    ascending=ascending_order,
+    inplace=True
+)
+
+# Quick sanity check
+print(datafile.head())
+print(datafile.info())
+
+# Example 1: Bar chart (top 10 categories)
+top_tracks = datafile[['track_popularity', 'track_name']].head(10)
+print("First 10 Rows:\n", top_tracks)
+
+plt.figure()
+top_tracks.plot.bar(
+    x='track_name',
+    y='track_popularity',
+    title='Top 10 Popular Tracks recently'
+)
+plt.title("Top 10 Popular Tracks from 2025")
+plt.xlabel("Track")
+plt.ylabel("Popularity")
+plt.xticks(rotation=45,ha='right')
+plt.tight_layout()
+plt.show()
